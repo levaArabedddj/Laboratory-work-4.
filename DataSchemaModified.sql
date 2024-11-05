1c1f300b830b88a59e30acfa857dcc7e4c802248
levaArabedddj <127316849+levaArabedddj@users.noreply.github.com>
2024-11-05 16:10:03 +0100

    Update DataSchemaModified.sql

--- DataSchemaModified.sql
+++ DataSchemaModified.sql
@@ -5,7 +5,7 @@ CREATE TABLE Conference (
     id INT PRIMARY KEY,
     participantCount INT CHECK (participantCount > 0),
     eventDate DATE CHECK (eventDate > CURRENT_DATE), 
-    location VARCHAR(100) CHECK (location ~ '^[A-Za-z0-9\\s,]+$'), 
+    location VARCHAR(100) CHECK (location ~ '^[A-Za-z0-9\\s,]'), 
     organizer VARCHAR(50),
     topic VARCHAR(100),
     duration FLOAT CHECK (duration > 0),
